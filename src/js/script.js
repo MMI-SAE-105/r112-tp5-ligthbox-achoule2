15becedf1dd3e047bf84ba8fe51ef56d9b04b1ab
achoule2 <150043943+achoule2@users.noreply.github.com>
2024-11-27 08:56:38 +0100

    ajout script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-// *** LIGHTBOX ***
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll("ul img");
+    const lightbox = document.querySelector("#lightbox");
+    const lightboxImg = lightbox.querySelector("img");
+  
+    images.forEach((img) => {
+      img.addEventListener("click", () => {
+        const fullImgSrc = img.getAttribute("data-full-img");
+        lightboxImg.src = fullImgSrc; 
+        lightbox.showModal(); 
+      });
+    });
+  
+    lightbox.addEventListener("click", (event) => {
+      if (!event.target.matches("img")) {
+        lightbox.close();
+      }
+    });
+  });
+  
