7b9f33a8e154e80a08d209f1ca102d19c55c1074
achoule2 <150043943+achoule2@users.noreply.github.com>
2024-12-04 08:14:04 +0100

    java script clic

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const images = document.querySelectorAll("ul img");
-    const lightbox = document.querySelector("#lightbox");
-    const lightboxImg = lightbox.querySelector("img");
-  
-    images.forEach((img) => {
-      img.addEventListener("click", () => {
-        const fullImgSrc = img.getAttribute("data-full-img");
-        lightboxImg.src = fullImgSrc; 
-        lightbox.showModal(); 
-      });
-    });
-  
-    lightbox.addEventListener("click", (event) => {
-      if (!event.target.matches("img")) {
-        lightbox.close();
-      }
-    });
+const thumbnails = document.querySelectorAll('ul li img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = lightbox.querySelector('img');
+
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener('click', () => {
+    const fullImgUrl = thumbnail.dataset.fullImg; 
+    lightboxImg.src = fullImgUrl; 
+    lightbox.showModal(); 
   });
-  
+});
+
+
+lightbox.addEventListener('click', (event) => {
+  if (event.target === lightbox) {
+    lightbox.close(); 
+  }
+});
